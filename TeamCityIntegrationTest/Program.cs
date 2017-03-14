3615ae13121a5d1b47fa92bece8fa6a14ff0180f
Alece Maharjan <timibhane@live.com>
2017-03-14 11:58:57 -0400

    Add project files.

---
+++ TeamCityIntegrationTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamCityIntegrationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test project to check continuous integartion");
+            Console.WriteLine("Test Slack and teamcity and git");
+            Console.ReadLine();
+            
+        }
+    }
+}
